fb741a0d946c887914ea6ed9e4ad67acb8cd035e
R0wyn <71075414+R0wyn@users.noreply.github.com>
2020-09-10 13:19:59 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,58 @@
+dice = 0
+
+def on_gesture_shake():
+    global dice
+    dice = randint(1, 6)
+    if dice == 1:
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            """)
+    elif dice == 2:
+        basic.show_leds("""
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            """)
+    elif dice == 3:
+        basic.show_leds("""
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            """)
+    elif dice == 4:
+        basic.show_icon(IconNames.HEART)
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            """)
+    elif dice == 5:
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            """)
+    elif dice == 6:
+        basic.show_leds("""
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            """)
+    else:
+        if dice == 4:
+            pass
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
